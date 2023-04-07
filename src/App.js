6d02faea3d43f1b291e09ca191b16e97a8a664c1
amol210 <114400251+amol210@users.noreply.github.com>
2023-04-07 15:22:50 +0530

    src
    src

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Navbar from './Components/Navbar';
+import RouteFile from './Components/RouteFile';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <Navbar/>
+    <RouteFile/>
+     
+    </div>
+  );
+}
+
+export default App;
